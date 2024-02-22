e010555679afae36f6984f6c56318411fd45c9b6
RokeCepedaArroita <47033174+RokeCepedaArroita@users.noreply.github.com>
2024-02-22 01:08:16 +0100

    Delete optimizer.py

--- optimizer.py
+++
@@ -1,9 +0,0 @@
-from optimal_departure import max_tidal_current_speed, river_max_current_speed, river_length, kayak_speed, max_tidal_current_speed
-import matplotlib.pyplot as plt
-from kayak import *
-from currents import *
-
-
-
-travel_time, travel_distance, times, distances, tidal_currents, river_currents, effective_speeds = \
-    simulate_trip(start_time, max_tidal_current_speed, river_max_current_speed, river_length, kayak_speed)
